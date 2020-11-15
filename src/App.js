4fb85e9a05ad7a21e8f2fc47dbd5b620b82bb8bc
houssam <houssammarrakchi3@gmail.com>
2020-11-15 15:00:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,21 @@
 import "./App.css";
-import Form from 'react-bootstrap/Form';
-import Col from "react-bootstrap/Col"
-import Button from "react-bootstrap/Button"
+import img from "../src/imageInSrc.png";
 
 function App() {
   return (
     <div className="App">
-      <Form>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Group controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
-
-        <Form.Group controlId="formGridAddress2">
-          <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
-        </Form.Group>
-
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Control as="select" defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Control>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Group id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <>
+        <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+          <h1 className="title red">Your name here</h1>
+          <br />
+          <img src={img} />
+          <br />
+          <img src="/imageInPublic.png" />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+      </>
     </div>
   );
 }
